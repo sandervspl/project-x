22c950e0224ed7370d7fa97760896a35bbf53ce4
Sander Vispoel <sandervispoel@hotmail.com>
2017-04-25 17:10:52 +0200

    Moved user data to Profile component

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,11 +1,69 @@
 // dependencies
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Loader } from 'semantic-ui-react';
+
+// components
+import Authorized from 'components/Authorized/Authorized';
+
+// actions
+import * as authActions from 'ducks/modules/auth';
 
 // style
 import './Profile.styl';
 
-const Profile = () => (
-  <h1>Profile</h1>
-);
+@connect(
+  state => ({ auth: state.app.auth }),
+  authActions,
+)
+class Profile extends Component {
+  static propTypes = {
+    auth: PropTypes.shape({
+      user: PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        username: PropTypes.string,
+        email: PropTypes.string,
+      }),
+    }),
+    fetchUserData: PropTypes.func,
+  };
+
+  componentWillMount() {
+    const { fetchUserData } = this.props;
+    const { user } = this.props.auth;
+
+    // fetch user data if its missing
+    if (!user) fetchUserData();
+  }
+
+  render() {
+    const { user } = this.props.auth;
+
+    if (!user) {
+      return (
+        <div>
+          <h1>Profile</h1>
+          <Loader className="purple-loader" active size="massive" />
+        </div>
+      );
+    }
+
+    const { firstName, lastName, username, email } = this.props.auth.user;
+    return (
+      <div>
+        <h1>Profile</h1>
+        <Authorized>
+          <ul>
+            <li>{firstName}</li>
+            <li>{lastName}</li>
+            <li>{username}</li>
+            <li>{email}</li>
+          </ul>
+        </Authorized>
+      </div>
+    );
+  }
+}
 
 export default Profile;
--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -1,82 +1,22 @@
 // dependencies
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import React, { PropTypes } from 'react';
 
-// components
-import Authorized from 'components/Authorized/Authorized';
-
-// actions
-import * as authActions from 'ducks/modules/auth';
+import NavigationBar from 'components/NavigationBar/NavigationBar';
 
 // style
 import './User.styl';
 
-@connect(
-  state => ({ auth: state.app.auth }),
-  authActions,
-)
-class User extends Component {
-  static propTypes = {
-    children: PropTypes.element,
-    auth: PropTypes.shape({
-      user: PropTypes.shape({
-        firstName: PropTypes.string,
-        lastName: PropTypes.string,
-        username: PropTypes.string,
-        email: PropTypes.string,
-      }),
-    }),
-    fetchUserData: PropTypes.func,
-  };
+const User = ({ children }) => (
+  <div className="page-fill">
+    <NavigationBar />
+    <div className="inner">
+      { children }
+    </div>
+  </div>
+);
 
-  state = {
-    load: false,
-  };
-
-  componentWillMount() {
-    const { fetchUserData } = this.props;
-    const { user } = this.props.auth;
-
-    if (!user) {
-      this.setState({ load: true });
-
-      fetchUserData()
-        .then(this.setState({ load: false }));
-    }
-  }
-
-  render() {
-    const { children } = this.props;
-    const { user } = this.props.auth;
-    const { load } = this.state;
-
-    if (load || !user) {
-      return (
-        <div className="page-fill">
-          <Dimmer>
-            <Loader active />
-            { children }
-          </Dimmer>
-        </div>
-      );
-    }
-
-    const { firstName, lastName, username, email } = this.props.auth.user;
-    return (
-      <div>
-        { children }
-        <Authorized>
-          <ul>
-            <li>{firstName}</li>
-            <li>{lastName}</li>
-            <li>{username}</li>
-            <li>{email}</li>
-          </ul>
-        </Authorized>
-      </div>
-    );
-  }
-}
+User.propTypes = {
+  children: PropTypes.element,
+};
 
 export default User;

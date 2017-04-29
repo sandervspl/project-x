f9d0397b73c65fc4d1c8d2381fc7b79fc8a15fa9
Sander Vispoel <sandervispoel@hotmail.com>
2017-04-29 19:54:06 +0200

    Fixed visual bugs on safari

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -9,9 +9,7 @@ import './NavigationBar.styl';
 const NavigationBar = ({ toggleSidebar }) => (
   <nav id="navbar">
     <Menu attached="top" color="black" className="top-nav">
-      <button onClick={toggleSidebar}>
-        <Icon className="sidebar-toggle" name="content" inverted />
-      </button>
+      <Icon className="sidebar-toggle" name="content" onClick={toggleSidebar} inverted />
       <Menu.Item name="Project-X" className="title">
         <Link to="/">Project-x</Link>
       </Menu.Item>

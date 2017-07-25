8b85a4514d54bc2577ea7d4c240fa7dd571118cd
Sander Vispoel <sandervispoel@hotmail.com>
2017-07-25 16:58:13 +0200

    PageFill now generates a classlist array

--- src/components/PageFill/PageFill.js
+++ src/components/PageFill/PageFill.js
@@ -1,5 +1,6 @@
 // dependencies
 import React, { PropTypes } from 'react';
+import { isEmpty } from 'lodash';
 
 // components
 import PageInner from 'components/PageInner/PageInner';
@@ -7,18 +8,28 @@ import PageInner from 'components/PageInner/PageInner';
 // style
 import './PageFill.styl';
 
-const PageFill = ({ children, inner, flow, className, id }) => (
-  <div id={id} className={`page-fill__container ${flow ? 'page-fill__container--flow' : ''} ${className}`}>
-    {do { /* eslint-disable */
-      if (inner) {
-        <PageInner> { children } </PageInner>;
-      } else {
-        children
-      }
-      /* eslint-enable */
-    }}
-  </div>
-);
+const PageFill = ({ children, inner, flow, className, id }) => {
+  const block = 'page-fill';
+  const classlist = [`${block}__container`];
+
+  if (flow) classlist.push(`${block}__container--flow`);
+  if (!isEmpty(className)) classlist.push(className);
+
+  const clsName = classlist.join(' ');
+
+  return (
+    <div id={id} className={clsName}>
+      {do { /* eslint-disable */
+        if (inner) {
+          <PageInner> {children} </PageInner>;
+        } else {
+          children;
+        }
+        /* eslint-enable */
+      }}
+    </div>
+  );
+};
 
 PageFill.propTypes = {
   // eslint-disable-next-line

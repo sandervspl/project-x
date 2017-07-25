9ae3992ab620d300cf76f74c5459fb67897e085d
Sander Vispoel <sandervispoel@hotmail.com>
2017-07-25 16:58:53 +0200

    Fixed broken welcome page styling and fixed avatar display

--- src/containers/Register/WelcomePage/WelcomePage.js
+++ src/containers/Register/WelcomePage/WelcomePage.js
@@ -2,6 +2,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import { isEmpty } from 'lodash';
 
 // components
 import Button from 'components/Button/Button';
@@ -9,13 +10,16 @@ import Icon from 'components/Icon/Icon';
 import PageFill from 'components/PageFill/PageFill';
 
 // assets
-import defaultImg from 'assets/images/avatar_default.png';
+import defaultImg from 'assets/images/avatars/avatar_default.png';
 
 // route paths
 import routes from 'routes/routes';
 
 // style
 import './WelcomePage.styl';
+
+// generate context path
+const pathToAvatars = require.context(process.env.REACT_APP_IMAGE_UPLOAD_PATH);
 
 const GetStartedButton = () => {
   const handleClick = () => {
@@ -24,27 +28,41 @@ const GetStartedButton = () => {
 
   return (
     <Button
-      className="get-started-btn"
+      className="welcome-page__get-started-btn"
       color="white"
       fontSize="big"
       fontColor="purple"
       onClick={handleClick}
     >
       GET STARTED
-      <Icon name="chevron-right" size="big" color="purple" className="get-started-icon" />
+      <Icon
+        name="chevron-right"
+        size="big"
+        color="purple"
+        className="welcome-page__get-started-icon"
+      />
     </Button>
   );
 };
 
 // TODO: fix avatar img src
-const WelcomePage = ({ user }) => (
-  <PageFill className="welcome-page">
-    <div id="portrait-container" style={{ backgroundImage: `url(${defaultImg})` }} />
-    <h1>Welcome</h1>
-    <h3>{user.username}</h3>
-    <GetStartedButton />
-  </PageFill>
-);
+const WelcomePage = ({ user }) => {
+  const img = !isEmpty(user.avatar) ? pathToAvatars(`./${user.avatar}`) : defaultImg;
+
+  return (
+    <PageFill className="welcome-page" inner={false}>
+      <div className="welcome-page__inner">
+        <div
+          className="welcome-page__portrait-container"
+          style={{ backgroundImage: `url(${img})` }}
+        />
+        <h1>Welcome</h1>
+        <h3>{user.username}</h3>
+        <GetStartedButton />
+      </div>
+    </PageFill>
+  );
+};
 
 WelcomePage.propTypes = {
   user: PropTypes.shape({}),

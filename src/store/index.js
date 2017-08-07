9bbcdc10726c4f26c958ec547f20b99a0c3b456d
Sander Vispoel <sandervispoel@hotmail.com>
2017-08-07 14:21:20 +0200

    added redux devtools support

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { routerMiddleware, routerReducer as routing } from 'react-router-redux';
 import { responsiveStateReducer } from 'redux-responsive';
 import { browserHistory } from 'react-router';
@@ -19,15 +19,15 @@ const reducer = combineReducers({
 // create store with middleware
 const logger = createLogger();
 
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  applyMiddleware(
-    thunk,
-    promise,
-    logger,
-    routerMiddleware(browserHistory),
+  composeEnhancers(
+    applyMiddleware(thunk, promise, logger, routerMiddleware(browserHistory)),
   ),
 );
+/* eslint-enable */
 
 // debugging
 if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
